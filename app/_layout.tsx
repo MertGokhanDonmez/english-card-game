254cf52437aa5af3f113b31afa0e3a086bb65b17
MertGokhanDonmez <mertgokhandonmez@gmail.com>
2025-04-23 21:33:07 +0300

    update status bar bg color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,15 +4,11 @@ import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <StatusBar style="dark" backgroundColor="#f2f2f2" translucent={false} />
-      <Stack.Screen name="index" />
-      <Stack.Screen
-        name="imageModal"
-        options={{
-          presentation: "modal",
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar style="auto" backgroundColor="#f2f2f2" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </>
   );
 }
